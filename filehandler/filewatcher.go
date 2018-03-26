2d991bc9d72cee47a6f33bb2609c2663ef2d7831
Christopher Fox <cfox1290@gmail.com>
2018-03-26 15:10:07 -0600

    startign to outline

---
+++ filehandler/filewatcher.go
@@ -0,0 +1,27 @@
+package filehandler
+
+
+import(
+	"fmt"
+	"os"
+	"time"
+)
+
+// func main(){
+// 	WatchFile("somename.txt")
+// }
+
+func WatchFile(fname string){
+	filestat1, err := os.Stat(fname)
+	PrintFatalError(err)
+	for{
+		time.Sleep(1 * time.Second)
+		filestat2, err := os.Stat(fname)
+		PrintFatalError(err)
+		if filestat1.ModTime() != filestat2.ModTime(){
+			fmt.Println("File was modified at", filestat2.ModTime())
+			filestat1, err = os.Stat(fname)
+			PrintFatalError(err)
+		}
+	}
+}

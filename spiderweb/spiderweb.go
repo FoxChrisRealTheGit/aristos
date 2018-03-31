bf4863578b8707c77b86d5282e8ba2dcd16c2153
Christopher Fox <cfox1290@gmail.com>
2018-03-30 19:22:59 -0600

    starting to outline the editor

--- spiderweb/spiderweb.go
+++ spiderweb/spiderweb.go
@@ -13,11 +13,15 @@ import(
 
 func Run(){
 	r := mux.NewRouter()
+	r.HandleFunc("/", handlers.HomeHandler).Methods("GET")
 	r.HandleFunc("/signup", handlers.SignUpHandler).Methods("GET", "POST")
+	r.HandleFunc("/editor", handlers.EditorHandler).Methods("GET")
+	r.HandleFunc("/editor/image-upload", handlers.UploadImageHandler).Methods("GET", "POST")
+	r.HandleFunc("/editor/video-upload", handlers.UploadVideoHandler).Methods("GET", "POST")
 
-
-	//middleware stuffs
+	// middleware stuffs
 	http.Handle("/", middleware.PanicRecoveryHandler(ghandlers.LoggingHandler(os.Stdout, r)))
 
+	// basic server stuffs
 	http.ListenAndServe(":8080", nil)
 }
